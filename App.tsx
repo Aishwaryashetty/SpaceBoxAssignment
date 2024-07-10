99c45c5e7a70188e5de112e7b4c6372187b55b74
Aishwaryashetty <aishwaryapshetty@gmail.com>
2024-07-10 23:46:38 +0530

    features done, Whats left - UI fixes,error handling and automation testing

--- App.tsx
+++ App.tsx
@@ -8,16 +8,22 @@ import {
   Platform,
   Alert,
   SafeAreaView,
+  FlatList
 } from 'react-native';
 import { v4 as uuidv4 } from 'uuid';
 
+
 import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
 import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import storage from '@react-native-firebase/storage';
 
 const App = () => {
   const [imageSource, setImageSource] = useState(null);
   const [showCamera, setShowCamera] = useState(false);
   const [images, setImages] = useState<string[]>([]);
+
+  const reference = storage().ref('/images');
+
 
   useEffect(() => {
     checkPermissions();
@@ -49,7 +55,7 @@ const App = () => {
 
   const generateFileName = () => {
     const uuid = uuidv4();
-    return `${uuid}.jpg`;
+    return `${uuid}.jpg`; // Adjust file extension based on your requirements
   };
 
   const requestPermissions = async () => {
@@ -57,8 +63,8 @@ const App = () => {
       const cameraPermission = await request(PERMISSIONS.IOS.CAMERA);
       const photoPermission = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
       console.log('cameraPermission', cameraPermission);
-      console.log('photoPermission', photoPermission);
-      console.log('RESULTS', RESULTS);
+      console.log('photoPermission', photoPermission)
+      console.log('RESULTS', RESULTS)
 
       if (
         cameraPermission !== RESULTS.GRANTED ||
@@ -88,6 +94,18 @@ const App = () => {
     }
   };
 
+  const uploadImage = async () => {
+    const fileName = generateFileName();
+
+    const reference = storage().ref(`images/${fileName}`);
+    try {
+      await reference.putFile(imageSource!);
+      Alert.alert('Success', 'Image uploaded successfully!');
+    } catch (error) {
+      console.error('Error uploading image: ', error);
+      Alert.alert('Error', 'Failed to upload image.');
+    }
+  };
 
   const handleOpenGallery = async () => {
     setShowCamera(true);
@@ -140,10 +158,55 @@ const App = () => {
     setImageSource(null);
   };
 
+  const handleUploadImage = async () => {
+    if (!imageSource) {
+      return;
+    }
+    const task = reference.putFile(imageSource);
+
+    task.on('state_changed', taskSnapshot => {
+      console.log(`${taskSnapshot.bytesTransferred} transferred out of ${taskSnapshot.totalBytes}`);
+    });
+
+    task.then(() => {
+      console.log('Image uploaded to the bucket!');
+    });
+
+  };
+
+  const handleDownloads = async () => {
+    const url = await storage().ref('/images').getDownloadURL();
+
+  }
+
+  const fetchImages = async () => {
+    setImageSource(null);
+    setShowCamera(false);
+    const listRef = storage().ref('images/');
+    try {
+      const result = await listRef.listAll();
+      const urls = await Promise.all(result.items.map((imageRef) => imageRef.getDownloadURL()));
+      setImages(urls);
+    } catch (error) {
+      console.error('Error fetching images: ', error);
+      Alert.alert('Error', 'Failed to fetch images.');
+    }
+  };
+
+  const renderImage = ({ item }) => (
+    <View style={{ margin: 5 }}>
+      <Image source={{ uri: item }} style={{ width: 200, height: 200 }} />
+    </View>
+  );
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{ fontSize: 30, paddingLeft: 20 }}>Hey, Good Day!</Text>
       <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+
+        {/* selected image preview will shown else a dummy text */}
+
         {imageSource ? (
           <Image
             style={styles.previewImage}
@@ -155,24 +218,38 @@ const App = () => {
 
         <View style={styles.buttonContainer}>
           {!showCamera && (
-            <View>
+            <>
               <TouchableOpacity style={styles.button} onPress={handleOpenGallery}>
                 <Text style={styles.buttonText}>Open Gallery</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.button} onPress={handleOpenCamera}>
                 <Text style={styles.buttonText}>Open Camera</Text>
               </TouchableOpacity>
-            </View>
+              <TouchableOpacity style={styles.button} onPress={fetchImages}>
+                <Text style={styles.buttonText}>Fetch all uploaded Images</Text>
+              </TouchableOpacity>
+            </>
           )}
 
           {imageSource && (
-            <TouchableOpacity style={styles.button} onPress={handleRetake}>
-              <Text style={styles.buttonText}>Retake</Text>
-            </TouchableOpacity>
+            <>
+              <TouchableOpacity style={styles.button} onPress={handleRetake}>
+                <Text style={styles.buttonText}>Retake</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={handleUploadImage}>
+                <Text style={styles.buttonText}>Use Photo</Text>
+              </TouchableOpacity>
+            </>
           )}
 
           {images.length > 0 && (
             <View>
+              <FlatList
+                data={images}
+                renderItem={renderImage}
+                keyExtractor={(item, index) => index.toString()}
+                numColumns={2}
+              />
               <TouchableOpacity style={styles.button} onPress={handleRetake}>
                 <Text style={styles.buttonText}>Home</Text>
               </TouchableOpacity>
@@ -230,3 +307,4 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
